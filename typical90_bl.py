5fa7b43e06fbb8fba7657d11e7ebf0ecd561c298
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2021-07-29 03:46:36 +0900

    Update typical90_bl.py

--- typical90_bl.py
+++ typical90_bl.py
@@ -1,29 +1,24 @@
-import sys
-import numpy as np
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+diff = [0 for i in range(len(A) - 1)]
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+for i in range(len(A)-1):
+    diff[i] = A[i+1] - A[i]
 
-
-def from_read(dtype=np.int64):
-    return np.fromstring(read().decode(), dtype=dtype, sep=" ")
-
-
-def from_readline(dtype=np.int64):
-    return np.fromstring(readline().decode(), dtype=dtype, sep=" ")
-
-
-N, Q = from_readline()
-A = from_readline()
-diff = np.insert(np.append(np.diff(A), 0), 0, 0)
-
+ans = sum(map(abs, diff))
 for i in range(Q):
-    L, R, V = from_readline()
-    diff[L-1] = diff[L-1] + V
-    diff[R] = diff[R] - V
-    ans = 0
-    for i in range(len(diff[1:-1])):
-        ans += abs(diff[i+1])
+    L, R, V = map(int, input().split())
+
+    diff_abs = 0
+    if L-2 != -1:
+        diff_abs += abs(diff[L-2] + V) - abs(diff[L-2])
+        diff[L-2] = diff[L-2] + V
+
+    if R-1 != N-1:
+        diff_abs += abs(diff[R-1] - V) - abs(diff[R-1])
+        diff[R-1] = diff[R-1] - V
+
+    ans += diff_abs
     print(ans)
+   
 
