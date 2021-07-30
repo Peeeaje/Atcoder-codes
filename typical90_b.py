41d7c1f1eb4f52f572c2e264c7a3df4ae76c6f00
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2021-07-31 08:12:08 +0900

    solved

--- typical90_b.py
+++ typical90_b.py
@@ -1,17 +1,19 @@
-n = int(input())
-for i in range(2 ** n):
-    temp = str()
-    temp_n = 0
-    for j in range(n):
-        if (i >> j) & 1:
-            temp = ")" + temp
-            temp_n += 1
+N = int(input())
+
+for i in range(2 ** N):
+    b_i = bin(i)
+    s = str()
+    temp = 0
+
+    for j in range(N):
+        if ((i >> j) & 1):
+            temp += 1
+            s += ")"
         else:
-            temp = "(" + temp
-            temp_n -= 1
-
-        if temp_n < 0:
+            temp -= 1
+            s += "("
+        if temp < 0:
             break
-
-    if j == n - 1 and temp_n == 0:
-        print(temp)
+            
+    if temp == 0:
+        print(s[::-1])
