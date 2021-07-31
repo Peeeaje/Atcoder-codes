88f85249f25f62ec257c0072ac359aa9a135e545
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2021-07-31 22:57:35 +0900

    Update typical90_aa.py

--- typical90_aa.py
+++ typical90_aa.py
@@ -1,24 +1,11 @@
-import sys
-import numpy as np
+N = int(input())
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
-
-
-def from_read(dtype=np.int64):
-    return np.fromstring(read().decode(), dtype=dtype, sep=" ")
-
-
-def from_readline(dtype=np.int64):
-    return np.fromstring(readline().decode(), dtype=dtype, sep=" ")
-
-
-N = from_readline()[0]
-
-temp = set()
-for i in range(N):
+l = set()
+for i in range(1, N + 1):
     S = input()
-    if S not in temp:
-        print(i + 1)
-    temp.add(S)
+
+    if S in l:
+        pass
+    else:
+        l.add(S)
+        print(i)
