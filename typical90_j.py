2627ba38139026db7ad82fc16bf7cc684299ca09
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2021-07-31 09:24:53 +0900

    resolve

--- typical90_j.py
+++ typical90_j.py
@@ -1,35 +1,25 @@
-import sys
 import numpy as np
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+N = int(input())
+C1 = [0]
+C2 = [0]
 
-
-def from_read(dtype=np.int64):
-    return np.fromstring(read().decode(), dtype=dtype, sep=" ")
-
-
-def from_readline(dtype=np.int64):
-    return np.fromstring(readline().decode(), dtype=dtype, sep=" ")
-
-
-N = from_readline()[0]
-CP = np.array([], dtype=np.int64)
 for i in range(N):
-    CP = np.append(CP, from_readline())
-CP = CP.reshape(N, 2)
-Q = from_readline()[0]
-LR = from_read().reshape(Q, 2)
+    C, P = map(int, input().split())
 
-cumsum_1 = np.cumsum([a[1] if a[0] == 1 else 0 for a in CP])
-cumsum_2 = np.cumsum([a[1] if a[0] == 2 else 0 for a in CP])
-cumsum_1 = np.append(cumsum_1, 0)
-cumsum_2 = np.append(cumsum_2, 0)
+    if C == 1:
+        C1.append(P)
+        C2.append(0)
+    else:
+        C1.append(0)
+        C2.append(P)
 
-for i in LR:
-    L = i[1] - 1
-    R = i[0] - 2
-    ans_1 = cumsum_1[L] - cumsum_1[R]
-    ans_2 = cumsum_2[L] - cumsum_2[R]
-    print(ans_1, ans_2)
+C1 = np.cumsum(C1)
+C2 = np.cumsum(C2)
+Q = int(input())
+
+for i in range(Q):
+    l, r = map(int, input().split())
+    l, r = l - 1, r - 1
+
+    print(C1[r + 1] - C1[l], C2[r + 1] - C2[l])
