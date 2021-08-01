7de356380f36d5d3bbf9957e85d9ea89519b1f33
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2021-08-01 15:31:50 +0900

    resolved

--- typical90_ar.py
+++ typical90_ar.py
@@ -5,22 +5,25 @@ read = sys.stdin.buffer.read
 readline = sys.stdin.buffer.readline
 readlines = sys.stdin.buffer.readlines
 
-N, Q = map(int, input().split())
-A = list(map(int, input().split()))
 
-temp = 0
+def from_read(dtype=np.int64):
+    return np.fromstring(read().decode(), dtype=dtype, sep=" ")
 
+
+def from_readline(dtype=np.int64):
+    return np.fromstring(readline().decode(), dtype=dtype, sep=" ")
+
+
+N, Q = from_readline()
+A = from_readline()
+
+
+shift = 0
 for i in range(Q):
-    t = list(map(int, input().split()))
-
-    if t[0] == 2:
-        temp += 1
-
-    elif t[0] == 1:
-        A[(t[1] - temp - 1) % N], A[(t[2] - temp - 1) % N] = (
-            A[(t[2] - temp - 1) % N],
-            A[(t[1] - temp - 1) % N],
-        )
-
+    T, x, y = from_readline()
+    if T == 1:
+        A[x-1], A[y-1] = A[y-1], A[x-1]
+    if T == 2:
+        shift += 1
     else:
-        print(A[(t[1] - temp - 1) % N])
+        print(A[x - shift])
