b7649c45cbf8e9f4f3a556d9be3c00f0121409d5
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2022-04-14 17:39:36 +0900

    update

--- typical90_bk.py
+++ typical90_bk.py
@@ -4,25 +4,14 @@ P = []
 for i in range(H):
     P.append(list(map(int, input().split())))
 
-ans = []
-for i in range(2 ** H):
-    R = dict()
-    for j in range(H * W + 1):
-        R[j] = 0
-
-    array = []
+for i in range(2**H):
+    rows = []
     for j in range(H):
-        if (i >> j) & 1:
-            array.append(P[j])
-        else:
-            pass
+        if i >> j & 1:
+            rows.append(P[j])
+    same_columns = []
+    rows_T = list(zip(*rows))
 
-    array = [list(x) for x in zip(*array)]
-
-    for a in array:
-        if len(set(a)) == 1:
-            R[a[0]] += len(a)
-    ans.append(max(R.values()))
-
-print(max(ans))
-
+    for value in rows_T:
+        if len(set(value)) == 1:
+            same_columns.append(value[0])
