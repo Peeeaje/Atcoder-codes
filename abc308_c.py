d114fd84bfa45bfcdca9632e1ec7066b12d25ba1
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2023-12-27 00:30:05 +0900

    精進

--- abc308_c.py
+++ abc308_c.py
@@ -1,3 +1,21 @@
+from functools import cmp_to_key
+
+
+def sort_method(a, b):
+    a_i, a_a, a_b = a
+    b_i, b_a, b_b = b
+
+    x = a_a * (b_a + b_b)
+    y = b_a * (a_a + a_b)
+    if x > y:
+        value = -1
+    elif x < y:
+        value = 1
+    else:
+        value = 0
+    return value
+
+
 n = int(input())
 l = []
 
@@ -6,6 +24,6 @@ for i in range(n):
     l.append((i, a, b))
 
 
-l = sorted(l, key=lambda x: (x[1]/(x[1] + x[2]), -x[0]), reverse=True)
+l.sort(key=cmp_to_key(sort_method))
 ans = [x[0] + 1 for x in l]
 print(*ans)
